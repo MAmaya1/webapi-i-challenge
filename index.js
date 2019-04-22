f83cb58568210e64be195beec9218d8239219026
Mario Amaya <thefozcat@gmail.com>
2019-04-22 14:09:16 -0700

    added get endpoint

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // implement your API here
+
+const express = require('express');
+
+const db = require('./data/db');
+
+const server = express();
+
+// Get Request
+
+server.get('/users', (req, res) => {
+    db.find()
+        .then(users => {
+            res.json(users)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: 'The users information could not be retrieved'})
+        })
+})
+
+server.listen(5000, () => {
+    console.log('\n*** API running on port 5K ***\n');
+})
