5685ef847d33472a3f4b0b664b700c9a0ba3c3c3
Mario Amaya <thefozcat@gmail.com>
2019-04-22 15:00:52 -0700

    created POST

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const db = require('./data/db');
 
 const server = express();
 
-// Get Request
+server.use(express.json());
+
+// GET
 
 server.get('/api/users', (req, res) => {
     db.find()
@@ -27,6 +29,24 @@ server.get('/api/users/:id', (req, res) => {
         })
         .catch(err => {
             res.status(404).json({ error: err, message: 'The user with the specified ID does not exist.' })
+        })
+})
+
+// POST
+
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    if (!req.body.name && !req.body.bio) {
+        res.status(400).json({ errorMessage: "Please provide name and bio for the user." });
+    }
+
+    db.insert(newUser)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => {
+            res.status(500).json({ error: err, message: 'There was an error while saving the user to the database.'})
         })
 })
 
